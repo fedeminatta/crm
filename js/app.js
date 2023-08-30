d9a980f3dbd128b3efee5b2785e4c0f5c70b9064
fedeminatta <minattafederico@gmail.com>
2023-08-30 14:50:10 -0300

    estilos lime

--- js/app.js
+++ js/app.js
@@ -101,19 +101,19 @@
 					listadoClientes.innerHTML += `
 
                         <tr>
-                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
-                                <p class="text-sm leading-10 text-gray-700"> ${email} </p>
+                            <td class="px-6 py-8 border-b-2 border-lime-300  h-auto">
+                                <p class="text-sm leading-5 font-medium text-lime-200 text-lg  font-bold"> ${nombre} </p>
+                                <p class="text-sm leading-10 text-lime-200"> ${email} </p>
                             </td>
-                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
-                                <p class="text-gray-700">${telefono}</p>
+                            <td class="px-6 py-8 border-b-2 border-lime-300  h-auto">
+                                <p class="text-lime-200">${telefono}</p>
                             </td>
-                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200  leading-5 text-gray-700">    
-                                <p class="text-gray-600">${empresa}</p>
+                            <td class="px-6 py-8 border-b-2 border-lime-300 leading-5  h-auto text-gray-700">    
+                                <p class="text-lime-200">${empresa}</p>
                             </td>
-                            <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
-                                <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
+                            <td class="px-6 py-8 border-b-2 border-lime-300 h-auto text-sm leading-5">
+								<a href="editar-cliente.html?id=${id}" class="block mb-3 text-lime-700 bg-lime-200 p-1  rounded-sm hover:text-teal-900 mr-5">Editar</a>
+								<a href="#" data-cliente="${id}" class="text-rose-700 bg-rose-200 p-1 rounded-sm hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;
